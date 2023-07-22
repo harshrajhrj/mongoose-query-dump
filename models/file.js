3b2e8d128f654b9012fef46f079d6fe9e891d401
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-07-22 18:47:59 +0530

    added nested crud

--- models/file.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FileSchema = new mongoose.Schema({
-    fileId: String,
-    fileUrl: String,
-}, { collection: 'file', timestamps: true });
-
-module.exports = mongoose.model('File', FileSchema);
--- route/API.js
+++ route/API.js
@@ -4,8 +4,6 @@ app.get('/', (req, res) => {
     res.json({ message: "You're home" });
 });
 
-app.use('/file', require('./file'));
-app.use('/student', require('./student'));
 app.use('/fruit', require('./fruit'));
 
 module.exports = app;
