d7a6acf5a6c4bb32c693f50ce846fadfdbeb849a
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-07-25 18:15:31 +0530

    multiple files upload completed

--- route/file.js
+++ route/file.js
@@ -2,11 +2,15 @@ const app = require('express').Router();
 const Storage = require('../gridFs-storage/fileStorage');
 
 // baseURL /file/..
+app.get('/', (req, res) => {
+    res.render('multipleFile.ejs');
+})
+
 app.post('/', Storage.upload.single('file'), (req, res) => {
     res.json(req.file);
 });
 
-app.post('/multiple', Storage.upload.array('file'), (req, res) => {
+app.post('/multiple', Storage.upload.array('files'), (req, res) => {
     res.json(req.files);
 });
 
--- storage.js
+++ storage.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 require('./mongo-connect')();
 const express = require('express');
 const app = express();
+const path = require('path');
 
 /**
  * Middlewares
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.use('/', require('./route/API'));
 
